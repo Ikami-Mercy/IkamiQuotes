7546eef4bfab4bf78d8f040d3faa8c86f9e0709e
Tindi Mercy <mercyikami95@gmail.com>
2019-12-05 18:14:06 +0300

    Update HurlStackTest.java

--- app/src/test/java/com/myapplication/HurlStackTest.java
+++ app/src/test/java/com/myapplication/HurlStackTest.java
@@ -1,7 +1,6 @@
 package com.myapplication;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
