4850e033a852c3dea74d000ecbd693179bdf7848
Tindi Mercy <mercyikami95@gmail.com>
2019-12-05 18:31:10 +0300

    Update ExampleUnitTest.java

--- app/src/test/java/com/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/myapplication/ExampleUnitTest.java
@@ -1,7 +1,6 @@
 package com.myapplication;
 
 import org.junit.Test;
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
